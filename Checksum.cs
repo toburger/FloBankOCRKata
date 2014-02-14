fc9b980f5e4d4849e9d43a21694c2ee2b3772d4a
Tobias Burger <tobias_burger@hotmail.com>
2014-02-14 12:30:12 +0100

    implement checksum algorithm (stolen from my F# code)

--- Checksum.cs
+++ Checksum.cs
@@ -10,8 +10,54 @@ namespace Kata_OCR
     {
         public static bool IsValid(int number)
         {
-            // TODO: Write the test
-            return false;
+            /*
+             * // F# sample
+             * let checksum xs =
+             *     xs
+             *     |> List.rev
+             *     |> List.mapi ((+)1>>(*))
+             *     |> List.fold (+) 0
+             *     |> flip (%) 11
+             *     |> (=)0
+             */
+
+            var checksum =
+                number.GetDigits()
+                      .Selecti((i, d) => (d + 1) * i)
+                      .Aggregate(0, (s, d) => d + s);
+            return checksum % 11 == 0;
+        }
+    }
+
+    internal static class Int32Extensions
+    {
+        /// <summary>
+        /// Creates an IEnumerable of digits from an integer.
+        /// Eg. 1234 becomes {1,2,3,4}
+        /// </summary>
+        public static IEnumerable<int> GetDigits(this int number)
+        {
+            /*
+             * let digits n =
+             *     let digits n = int (floor (log10 (float n))) + 1
+             *     let digitInNumber n i = (n / (pown 10 i)) % 10
+             *     [ for i = (digits n)-1 downto 0 do yield digitInNumber n i ]
+             */
+
+            for (int i = (int)Math.Floor(Math.Log10(number)); i >= 0; i--)
+                yield return (int)(number / Math.Pow(10, i)) % 10;
+        }
+    }
+
+    internal static class EnumerableExtensions
+    {
+        public static IEnumerable<R> Selecti<T, R>(this IEnumerable<T> enumerable, Func<int, T, R> selector)
+        {
+            int i = 0;
+            foreach (var item in enumerable)
+            {
+                yield return selector(i++, item);
+            }
         }
     }
 }
--- Tests/ChecksumTests.cs
+++ Tests/ChecksumTests.cs
@@ -10,8 +10,35 @@ namespace Kata_OCR.Tests
 {
     public class ChecksumTests
     {
-        [Theory(Skip = "TODO: Implement a Checksum helper method")]
-        [InlineData(123456789, true)]
+        [Theory]
+        [InlineData(0, new int[0] { })]
+        [InlineData(1, new[] { 1 })]
+        [InlineData(1234, new[] { 1, 2, 3, 4 })]
+        [InlineData(0001234, new[] { 1, 2, 3, 4 })]
+        public void TestDigits(int number, int[] expected)
+        {
+            Assert.Equal(number.GetDigits(), expected);
+        }
+
+        [Fact]
+        public void TestSelecti()
+        {
+            var input = new[] { "a", "b", "c", "d" };
+            int counter = 0;
+            var result = input.Selecti((i, s) =>
+                {
+                    Assert.Equal(counter++, i);
+                    return i;
+                }).ToArray();
+            Assert.Equal(new[] { 0, 1, 2, 3 }, result);
+
+            var result2 = input.Selecti((i, s) => s);
+            Assert.Equal(new[] { "a", "b", "c", "d" }, result2);
+        }
+
+        [Theory]
+        [InlineData(457508000, true)]
+        [InlineData(111111111, false)]
         [InlineData(382913738, false)]
         public void TestValidChecksum(int number, bool isValid)
         {
