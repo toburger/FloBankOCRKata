cf0788774a93dfd3654d4dd82d66aa2a001beb00
Tobias Burger <tobias_burger@hotmail.com>
2014-02-17 11:12:03 +0100

    Refactor ParseDigits into a separate class

--- DigitsParser.cs
+++ DigitsParser.cs
@@ -3,38 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xunit;
 
-namespace Kata_OCR.Tests
+namespace Kata_OCR
 {
-    public class AccountNumberTests
+    static class DigitsParser
     {
-        [Fact]
-        public void TestAccountNumberParsing()
-        {
-            string accountNumberSample = @"
-    _  _  _  _  _  _  _  _ 
-|_||_   ||_ | ||_|| || || |
-  | _|  | _||_||_||_||_||_|";
-
-            //var accountNumber = new AccountNumber(accountNumberSample); // This should be the goal
-
-            int length = 9;
-
-            var digits = ParseDigits(accountNumberSample, length);
-            Assert.Equal(length, digits.Count());
-
-            var ns = 457508000.GetDigits().ToArray();
-            Assert.Equal(length, ns.Count());
-            for (int i = 0; i < length; i++)
-            {
-                int number;
-                digits[i].TryGetNumber(out number);
-                Assert.Equal(ns[i], number);
-            }
-        }
-
-        private static Digit[] ParseDigits(string accountNumber, int length = 9)
+        public static Digit[] ParseDigits(string accountNumber, int length = 9)
         {
             string[] lines = accountNumber.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
 
---
+++ Tests/DigitsParserTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kata_OCR.Tests
+{
+    public class DigitsParserTests
+    {
+        [Fact]
+        public void TestDigitsParsing()
+        {
+            string accountNumberSample = @"
+    _  _  _  _  _  _  _  _ 
+|_||_   ||_ | ||_|| || || |
+  | _|  | _||_||_||_||_||_|";
+
+            //var accountNumber = new AccountNumber(accountNumberSample); // This should be the goal
+
+            int length = 9;
+
+            var digits = DigitsParser.ParseDigits(accountNumberSample, length);
+            Assert.Equal(length, digits.Count());
+
+            var ns = 457508000.GetDigits().ToArray();
+            Assert.Equal(length, ns.Count());
+            for (int i = 0; i < length; i++)
+            {
+                int number;
+                digits[i].TryGetNumber(out number);
+                Assert.Equal(ns[i], number);
+            }
+        }
+    }
+}
