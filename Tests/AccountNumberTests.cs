a69cc6e38c92d681cf26bc215b5720f888d4093a
Tobias Burger <tobias_burger@hotmail.com>
2014-02-17 11:38:38 +0100

    Test AccountNumbers in sample file

--- Tests/AccountNumberTests.cs
+++ Tests/AccountNumberTests.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Kata_OCR.Tests
@@ -20,6 +18,24 @@ namespace Kata_OCR.Tests
             var accountNumber = new AccountNumber(accountNumberSample);
 
             Assert.Equal("457508000", accountNumber.GetNumber());
+        }
+
+        [Fact]
+        public void TestAccountNumbersFromSampleFile()
+        {
+            string[] lines = File.ReadAllLines("sample.txt");
+            var ans = new List<string>();
+            for (int i = 0; i < lines.Length; i += 4)
+            {
+                var accountNumber = string.Join("\n", new[] { lines[i], lines[i + 1], lines[i + 2] });
+                ans.Add(accountNumber);
+            }
+
+            var accountNumbers = ans.Select(an => new AccountNumber(an)).ToArray();
+
+            Assert.Equal(56, accountNumbers.Length);
+            Assert.Equal("457508000", accountNumbers[0].GetNumber());
+            Assert.Equal("012345678", accountNumbers[5].GetNumber());
         }
     }
 }
