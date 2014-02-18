730f505eef2dde7cecb9777f94efcf8146d5042c
Tobias Burger <tobias_burger@hotmail.com>
2014-02-18 08:31:10 +0100

    Use Linq for parsing Digits

--- Library/DigitsParser.cs
+++ Library/DigitsParser.cs
@@ -12,29 +12,18 @@ namespace Kata_OCR.Library
         {
             string[] lines = accountNumber.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
 
-            var lls = new List<List<string>>();
-            foreach (string line in lines)
-            {
-                var ls = new List<string>();
-                for (int i = 0; i < length * 3; i += 3)
-                {
-                    ls.Add(line.Substring(i, 3));
-                }
-                lls.Add(ls);
-            }
+            var lls = lines
+                .Select(line =>
+                    Enumerable.Range(0, length)
+                              .Select(i => line.Substring(i * 3, 3))
+                              .ToArray())
+                .ToArray();
 
-            string[] digits = new string[length];
-            for (int j = 0; j < lls.Count; j++)
-            {
-                for (int i = 0; i < length; i++)
-                {
-                    if (digits[i] == null) digits[i] = "";
-                    digits[i] += lls[j][i];
-                }
-            }
-
-            return digits.Select(digitAsString => new Digit(digitAsString))
-                         .ToArray();
+            return Enumerable
+                .Range(0, length)
+                .Select(i => lls[0][i] + lls[1][i] + lls[2][i])
+                .Select(digitAsString => new Digit(digitAsString))
+                .ToArray();
         }
 
         public static int[] GetNearestMatch(string digit)
